ac7ffe75ef3e0ef0fd6e982b8217317c0c3174b2
Huseyin Ergin <mersinus@gmail.com>
2017-10-30 22:34:43 -0400

    initial project files

---
+++ src/edu/bsu/cs/hergin/cs691/checkstyle/atM.java
@@ -0,0 +1,46 @@
+package edu.bsu.cs.hergin.cs691.checkstyle;
+
+import java.util.*;
+
+public class atM {
+
+	private double balance;
+
+	public double get_balance() {
+		return balance;
+	}
+
+/*
+ * This class is licensed with GPL.
+ */
+	
+	public atM(double ___initial_balance) {
+		this.balance = ___initial_balance;
+	}
+
+	public boolean deposit(double amnt) {
+		if (amnt < 0)
+			return false;
+	
+		this.balance += amnt;
+		return true;
+	}
+
+	// this class does withdraw
+	public boolean withDRWa(double amount) {
+		if (amount < 0) {
+			return false;
+		}
+		if (this.balance < amount) {
+			return false;
+		}
+		this.balance -= amount; return true;
+	}
+	
+	public void doNothing() 
+	{
+		
+		
+	}
+	
+}
